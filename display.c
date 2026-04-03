62189af27bde1b68c7c66117960b695807487b7a
Pilou-C <pierrelouiscoulon1@gmail.com>
2026-04-03 10:48:52 +0200

    Add game rules and grid management functions
    Implemented the rules and grid display functions for the game.

--- display.c
+++ display.c
@@ -1,21 +1,37 @@
 #include "display.h"
 
 void rules() {
-    // TO BE DONE
+    printf("=== MASTERMIND ===\n");
+    printf("Find the %d-digit combination. Each digit is between 1 and %d.\n", SIZE_COMB, NB_COLORS);
+    printf("Feedback rules:\n");
+    printf(" 'o' : Correct digit and position\n");
+    printf(" 'x' : Correct digit but wrong position\n");
+    printf(" '-' : Wrong digit\n");
+    printf("You have %d attempts. Good luck!\n", MAX_ATTEMPTS);
 }
 
 void init_grid(char * grid[]) {
-    // TO BE DONE
+    for (int i = 0; i < MAX_ATTEMPTS; i++) {
+        grid[i] = (char *)malloc(18 * sizeof(char));
+        strcpy(grid[i], "- - - - | - - - -");
+    }
 }
 
 void free_grid(char * grid[]) {
-    // TO BE DONE
+    for (int i = 0; i < MAX_ATTEMPTS; i++) {
+        free(grid[i]);
+    }
 }
 
 void update_grid(char * grid[], int attempts, int propos[], char result[]) {
-    // TO BE DONE
+    for (int i = 0; i < SIZE_COMB; i++) {
+        grid[attempts][i * 2] = propos[i] + '0';
+        grid[attempts][10 + i * 2] = result[i];
+    }
 }
 
 void display_grid(char * grid[]) {
-    // TO BE DONE
+    for (int i = 0; i < MAX_ATTEMPTS; i++) {
+        printf("Attempt %d: %s\n", i + 1, grid[i]);
+    }
 }

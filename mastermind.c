6eefb03bc8bfd8ca2335dc3489686cfa52d154af
Pilou-C <pierrelouiscoulon1@gmail.com>
2026-04-03 10:48:04 +0200

    Implement core functionality for mastermind game
    Implemented functions to display, read, generate random combinations, check results, and count well-placed numbers in the mastermind game.

--- mastermind.c
+++ mastermind.c
@@ -1,27 +1,51 @@
 #include "mastermind.h"
 
 void display_comb(int comb[]) {
-    // TO BE DONE
+    for (int i = 0; i < SIZE_COMB; i++) {
+        printf("%d ", comb[i]);
+    }
+    printf("\n");
 }
 
 void read_comb(int arr[]) {
-    // TO BE DONE
+    for (int i = 0; i < SIZE_COMB; i++) {
+        scanf("%d", &arr[i]);
+    }
 }
 
 void random_comb(int comb[]) {
-    // TO BE DONE
+    for (int i = 0; i < SIZE_COMB; i++) {
+        comb[i] = (rand() % NB_COLORS) + 1;
+    }
 }
 
 int present(int comb[], int number) {
-    // TO BE DONE
-    return -1;
+    for (int i = 0; i < SIZE_COMB; i++) {
+        if (comb[i] == number) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 void check_result(int comb[], int propos[], char result[]) {
-    // TO BE DONE
+    for (int i = 0; i < SIZE_COMB; i++) {
+        if (propos[i] == comb[i]) {
+            result[i] = 'o';
+        } else if (present(comb, propos[i])) {
+            result[i] = 'x';
+        } else {
+            result[i] = '-';
+        }
+    }
 }
 
 int well_placed(int comb[], int propos[]) {
-    // TO BE DONE
-    return -1;
+    int count = 0;
+    for (int i = 0; i < SIZE_COMB; i++) {
+        if (comb[i] == propos[i]) {
+            count++;
+        }
+    }
+    return count;
 }
